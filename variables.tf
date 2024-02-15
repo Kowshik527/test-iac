c10a2fb723b303804fd42f5ac9c8b913a94a682b
Kowshik527 <113774345+Kowshik527@users.noreply.github.com>
2024-02-15 11:29:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "ami_id" {
   type = string
   description = "The ID of the AMI to use for the instance"
-  default = "${amiId}"
+  default = "ami.1234555"
 }
 
 variable "instance_type" {
   type = string
   description = "The type of instance to launch"
-  defualt = "${instanceType}"
+  defualt = "t3,smallllllllll"
 }
 
 variable "instance_name" {
   type = string
   description = "The name of the instance"
-  default = "${instanceName}"
+  default = "myinnss"
 }
